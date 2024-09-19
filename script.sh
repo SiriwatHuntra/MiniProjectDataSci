884e53a270a1f3549faf8ab7a99ac0fd98fdac8f
Kantinan Boonyingsathit <kaseam008@gmail.com>
2024-09-20 00:28:58 +0700

    change the script file

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
 git clone https://github.com/SiriwatHuntra/MiniProjectDataSci.git
-
 cd MiniProjectDataSci/
-chmod -R 777 .
-./script.sh
 
+sudo apt install python3-venv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install gradio
+pip install scikit-learn
+python3 Gradio.py
