cccac68cfd6348a049b8931ded4a8d4d382a314d
mery9 <86040156+mery9@users.noreply.github.com>
2024-09-23 14:03:40 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
+# Clone the GitHub repository
 git clone https://github.com/SiriwatHuntra/MiniProjectDataSci.git
-cd MiniProjectDataSci/
 
-sudo apt install python3-venv
+# Navigate into the project directory
+cd MiniProjectDataSci
+
+# Update the package list
+sudo apt update
+
+# Install the Python virtual environment package
+sudo apt install -y python3-venv
+
+# Create a virtual environment
 python3 -m venv myenv
+
+# Activate the virtual environment
 source myenv/bin/activate
-pip install gradio
-pip install scikit-learn
-python3 Gradio.py
+
+# Install required Python packages
+pip install -r requirements.txt
+
+
+# Run the Streamlit application
+streamlit run Streamlit.py
