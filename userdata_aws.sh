106cb284cca7641d31d4e0e6d43e614be93ba56b
mery9 <86040156+mery9@users.noreply.github.com>
2024-09-23 14:13:19 +0700

    Update and rename script.sh to userdata_aws.sh

---
+++ userdata_aws.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Update the package list
+sudo apt update
+
+# Install Python3 and virtual environment
+sudo apt install -y python3-venv git
+
+# Clone your repository
+git clone https://github.com/SiriwatHuntra/MiniProjectDataSci.git
+
+# Navigate to the project directory
+cd MiniProjectDataSci
+
+# Set up the virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Install required packages
+pip install -r requirements.txt
+
+# Optionally, run a script like `streamlit` or another task
+streamlit run Streamlit.py
